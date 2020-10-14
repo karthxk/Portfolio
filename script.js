ded261b0349d9954265688af906bfa16937e7917
Karthik G <kgnaircochin02@gmail.com>
2020-10-14 23:48:15 +0530

    fixed navbar hide

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-$(".navbar").hide();
-  $(window).scroll(function () {
-    if ($(window).scrollTop() > 40) {
-      $(".navbar").slideDown(100);
-    } else {
-      $(".navbar").slideUp(100);
+$(function () {
+    $(".navbar").hide();
+    $(window).scroll(function () {
+            if ($(window).scrollTop() > 40) {
+             $(".navbar").slideDown(100);
+         } else {
+              $(".navbar").slideUp(100);
     }
   });
+});
